1c156214207fccc184c4b9da57cd8d189f3091b9
mukeshkaushal2008 <mukeshkaushal2008@gmail.com>
2018-07-14 17:00:38 +0530

    Test commit

--- config/maneuver.php
+++ config/maneuver.php
@@ -51,7 +51,7 @@ return array(
              'host'      => '127.0.0.1',
             'user'      => 'laravel',
             'pass'      => 'laravel@123',
-            'path'      => '/Change in ',
+            'path'      => '/',
             'port'      => 21,
             'passive'   => true
         ),
